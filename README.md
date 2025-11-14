c0a6ad543bde3e7185d622f28877fc2752e65309
Florian Pfleiderer <florian@pfleiderer.at>
2025-11-14 17:26:18 +0100

    build works using cmake

---
+++ README.md
@@ -0,0 +1,4 @@
+# Robot Control Sim
+
+This app simulates a PID controller that steers a "robot" towards a point in a 2D coordinate system.
+
---
+++ app/main.cpp
@@ -0,0 +1,8 @@
+// Created on Fri Nov 14 2025 by Florian Pfleiderer
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
